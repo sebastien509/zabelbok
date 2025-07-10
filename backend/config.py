e46fab01b9c429d5f65f6773a481f4283d16ba05
Sebastien <fenelonsebastien@outlook.com>
2025-07-09 23:31:07 -0400

    docker

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///eduplatform.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://postgres:uWnVZogsYTSpEhqjgpSeBmrmONjJxsfl@gondola.proxy.rlwy.net:58252/railway', 'sqlite:///eduplatform.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'supersecret')
     SWAGGER = {
